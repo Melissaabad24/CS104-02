19f48661358bd6306f873d20d1d481d8ba5b9032
Melissaabad24 <98341726+Melissaabad24@users.noreply.github.com>
2022-10-02 11:23:06 -0400

    conditions.py

---
+++ conditions.py
@@ -0,0 +1,24 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+a= temp
+input("Please enter the current temperature")
+
+if a > 90
+    print ("Wear shorts")
+
+    Else, if a > 70:
+        print ("Short sleeves aere fine")
+
+    Else, if a > 50:
+        print ("Wear a jacket")
+
+    Else, if a > 32:
+        print ("Wear a heavy coat")
+
+    Else:
+        print ("Stay inside")
+
+
+
+
+    
